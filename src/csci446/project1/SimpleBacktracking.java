c08fdca632f0aebc59a3242dad24dc0d02ce0f82
Chris Thomas <faore@onhaven.net>
2016-09-17 12:00:53 -0600

    Backtracking Skeleton

--- src/csci446/project1/SimpleBacktracking.java
+++ src/csci446/project1/SimpleBacktracking.java
@@ -4,4 +4,10 @@ package csci446.project1;
  * Created by cetho on 9/1/2016.
  */
 public class SimpleBacktracking {
+    private int[] colors;
+    private int numColors;
+
+    public SimpleBacktracking(int numColors) {
+
+    }
 }
